ab4f6a2554e34d818990ce93ff5bfa86f2c1e0f7
Reece Crook <reececrook16@gmail.com>
2023-04-07 19:26:33 -0600

    Worked but not passed, commiting for TC to see

--- main.js
+++ main.js
@@ -3,7 +3,33 @@ const EMPTY_HEART = '♡'
 const FULL_HEART = '♥'
 
 // Your JavaScript code goes here!
+const like = document.getElementsByClassName("like-glyph");
 
+const modal = document.getElementById('modal');
+modal.hidden = true
+
+for (let i = 0; i < like.length; i++) {
+  like[i].addEventListener("click", function(){
+    mimicServerCall()
+    .then(response => response)
+    .catch(() => {
+      modal.hidden = false, setTimeout(() => modal.hidden = true, 3000)
+      throw new Error('error');
+    })
+    .then(function() {
+      if(like[i].innerHTML === EMPTY_HEART){
+        like[i].innerHTML = FULL_HEART
+        like[i].classList.add('activated-heart')
+        } else{
+           like[i].innerHTML = EMPTY_HEART
+           like[i].classList.remove('activated-heart')
+          }
+          
+        });
+      })
+    }
+    
+  
 
 
 
